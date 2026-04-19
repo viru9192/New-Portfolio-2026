e1dbcc8bdc94a65eb2b5ca944c6daabf2f0531f5
viru9192 <viranchimore2112@gmail.com>
2026-04-18 23:35:09 -0400

    setup gh pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Updated-Portfolio/',
+  base: '/New-Portfolio-2026/',
   server: { port: 3000 },
 })
